e0d3d655a5c727445e6dad318c10cfeaa0f59393
root <root@localhost.localdomain>
2020-08-20 11:13:18 +0530

    Staring httpd template

--- chef_config_mng/cookbooks/media_wiki/recipes/httpd_start.rb
+++ chef_config_mng/cookbooks/media_wiki/recipes/httpd_start.rb
@@ -3,4 +3,12 @@ template '/etc/httpd/conf/httpd.conf' do
     owner 'root'
     group 'root'
     mode '0755'
+    notifies :run, 'execute[start apache]', :immediately
+ end
+
+ execute 'start apache' do
+    command "service httpd start"
+    owner 'root'
+    group 'root'
+    action :nothing
  end
